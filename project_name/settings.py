68ef1d3af8152673faa818545401bf5c34c1eb2a
unknown <gregmasterbr@gmail.com>
2023-02-02 15:13:22 -0300

    Removido o parâmetro default do allowed_hosts

--- project_name/settings.py
+++ project_name/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
